410d65e55064da082646f1a56e7280b64b6e0a1a
Amal Bose <axatrikx@gmail.com>
2017-03-25 20:50:00 +0530

    initial commit

---
+++ src/main.js
@@ -0,0 +1,10 @@
+const electron = require('electron');
+const{app, BrowserWindow} = electron;
+
+app.on('ready', () => {
+    let win = new BrowserWindow({
+        width: 1000,
+        height: 600
+    });
+    win.loadURL(`file://${__dirname}/index.html`);
+});
